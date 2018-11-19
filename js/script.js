9e366e27239e09d862c765ae80f96882cbe7cf85
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2018-11-20 00:55:39 +0100

    Add Front-End

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-
+$(document).ready(function(){
+		
+		$("#oneButton").on({
+			   mouseenter: function(){
+			    $('#oneIcon').attr('src','images/one-tile-white.png');
+			  },
+			  mouseleave: function(){
+			    $('#oneIcon').attr('src','images/one-tile-color.png');
+			  }
+			  });
+		
+		$("#fourButton").on({
+			   mouseenter: function(){
+			    $('#fourIcon').attr('src','images/four-tile-white.png');
+			  },
+			  mouseleave: function(){
+			    $('#fourIcon').attr('src','images/four-tile-color.png');
+			  }
+			  });
+		
+		$("#nineButton").on({
+			   mouseenter: function(){
+			    $('#nineIcon').attr('src','images/nine-tile-white.png');
+			  },
+			  mouseleave: function(){
+			    $('#nineIcon').attr('src','images/nine-tile-color.png');
+			  }
+			  });	  
+	});
